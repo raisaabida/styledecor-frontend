896d54c1d36bec2d8064fe9efc9069f74cf319e8
raisaabida <231011112@eastdelta.edu.bd>
2025-12-22 13:23:21 +0600

    eleven no push

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ErrorPage() {
+
+export default function ErrorPage()
+ {
   return (
     <div className="min-h-[70vh] flex flex-col items-center justify-center text-center">
       <h1 className="text-7xl font-bold text-teal-500">404</h1>
