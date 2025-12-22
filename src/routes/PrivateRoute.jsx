937ddcfb719e31a8cbf3ee969485c974314a18b8
raisaabida <231011112@eastdelta.edu.bd>
2025-12-22 13:27:27 +0600

    eighteen no push

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { auth } from "../firebase";
 
+
 const PrivateRoute = ({ children }) => {
   const user = auth.currentUser;
   return user ? children : <Navigate to="/login" />;
